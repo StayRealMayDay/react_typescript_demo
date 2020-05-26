360fbfaa46b6aab58f034415af879a163bdaa9ea
IMP <stayrealrhr@outlook.com>
2020-05-27 02:43:28 +0800

    add immutability-helper

--- craco.config.js
+++ craco.config.js
@@ -6,7 +6,7 @@ module.exports = {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
-          modifyVars: { '@primary-color': '#1DA57A' },
+          modifyVars: { '@primary-color': '#13c2c2' },
           javascriptEnabled: true,
         },
       },
